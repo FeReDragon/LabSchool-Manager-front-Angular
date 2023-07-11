da3c0aedf7e63070fa78df19c722c1239de5e6de
“FeReDragon” <f3030ma@gmail.com>
2023-07-11 16:05:53 -0300

    atualiz app comp- auth components

--- LABSchool_Manager/src/app/app-routing.module.ts
+++ LABSchool_Manager/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full'
+  },
+  // outras rotas vão aqui...
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
