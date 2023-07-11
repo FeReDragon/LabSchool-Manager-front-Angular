c65bfc58f1146a136f8f3a4aab47c03c69aec88f
“FeReDragon” <f3030ma@gmail.com>
2023-07-11 16:04:55 -0300

    auth routing e module

--- LABSchool_Manager/src/app/auth/auth-routing.module.ts
+++ LABSchool_Manager/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' } // redireciona para 'login' por padrão
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
