e4fe6cc97e3db99873f7635c50ee24443cc206f4
“FeReDragon” <f3030ma@gmail.com>
2023-07-11 16:03:20 -0300

    login component

--- LABSchool_Manager/src/app/auth/login/login.component.ts
+++ LABSchool_Manager/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  isLoading: boolean = false;
+  errorMessage: string = '';
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login(): void {
+    this.isLoading = true;
+    this.authService.login(this.email, this.password).subscribe(
+      () => {
+        this.isLoading = false;
+        this.router.navigate(['/']);
+      },
+      error => {
+        this.isLoading = false;
+        this.errorMessage = 'Invalid email or password';
+      }
+    );
+  }
+
+  goToRegister(): void {
+    this.router.navigate(['/register']);
+  }
 }
