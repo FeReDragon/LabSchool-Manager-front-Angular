58a2e20b7776a13f63adc03279ed00db9b9f36c6
“FeReDragon” <f3030ma@gmail.com>
2023-07-14 00:19:09 -0300

    atualizando btn logout

--- LABSchool_Manager/src/app/core/header/header.component.ts
+++ LABSchool_Manager/src/app/core/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  currentUser: any;
+  
+  constructor(private authService: AuthService, private router: Router) { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);  // supondo que sua rota de login seja '/login'
+  }
 }
