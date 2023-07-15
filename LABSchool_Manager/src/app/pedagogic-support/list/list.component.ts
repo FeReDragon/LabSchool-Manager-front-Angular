63b789911f03797a2f80ab5298e407ff83788d35
“FeReDragon” <f3030ma@gmail.com>
2023-07-15 09:04:32 -0300

    pedagogic listagem dinamica

--- LABSchool_Manager/src/app/pedagogic-support/list/list.component.ts
+++ LABSchool_Manager/src/app/pedagogic-support/list/list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { PedagogicSupportService } from '../../services/pedagogic-support.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  acompanhamentos$: Observable<any> = of();
 
+  constructor(private pedagogicSupportService: PedagogicSupportService) { }
+
+  ngOnInit() {
+    this.acompanhamentos$ = this.pedagogicSupportService.getAcompanhamentos();
+  }
+
+  trackByFn(index: number, item: any) {
+    return item.id;
+  }
 }
+
--- LABSchool_Manager/src/app/services/pedagogic-support.service.ts
+++ LABSchool_Manager/src/app/services/pedagogic-support.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PedagogicSupportService {
+  private API_URL = 'http://localhost:3000/acompanhamentos';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAcompanhamentos(): Observable<any> {
+    return this.http.get(this.API_URL);
+  }
 }
