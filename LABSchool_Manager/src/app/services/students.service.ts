f24ab45a021d8e9ec5c3479951a85d6cf8116a15
“FeReDragon” <f3030ma@gmail.com>
2023-07-15 01:03:45 -0300

    Lista alunos component - service

--- LABSchool_Manager/src/app/services/students.service.ts
+++ LABSchool_Manager/src/app/services/students.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StudentsService {
+export class StudentService {
+  private baseUrl = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getStudents(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/alunos`);
+  }
 }
--- LABSchool_Manager/src/app/students/list/list.component.ts
+++ LABSchool_Manager/src/app/students/list/list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../../services/students.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  students: any;
 
+  constructor(private studentService: StudentService) { }
+
+  ngOnInit(): void {
+    this.getStudents();
+  }
+
+  getStudents(): void {
+    this.studentService.getStudents().subscribe((data: any) => {
+      this.students = data;
+    });
+  }
 }
