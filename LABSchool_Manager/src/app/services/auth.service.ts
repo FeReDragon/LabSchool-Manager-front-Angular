d385fd64ac30080ad444e593daff888b9bb72ef7
“FeReDragon” <f3030ma@gmail.com>
2023-07-14 22:33:33 -0300

    currentUser/nome usuario header

--- LABSchool_Manager/src/app/services/auth.service.ts
+++ LABSchool_Manager/src/app/services/auth.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
+  private currentUserSubject: BehaviorSubject<string | null>;
+  public currentUser: Observable<string | null>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<string | null>(this.getLocalUser());
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  private getLocalUser(): string | null {
+    const user = localStorage.getItem('currentUser');
+    return user ? JSON.parse(user)[0].username : null; 
+  }
 
   login(username: string, password: string): Observable<boolean> {
     return this.http.get<any>(`http://localhost:3000/users?username=${username}&password=${password}`)
       .pipe(map(user => {
         if (user && user.length) {
           localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user[0].username);
           return true;
         }
         return false;
@@ -25,6 +37,8 @@ export class AuthService {
     return this.http.post<any>(`http://localhost:3000/users`, user)
       .pipe(map(response => {
         if (response) {
+          localStorage.setItem('currentUser', JSON.stringify(response));
+          this.currentUserSubject.next(response.username);
           return true;
         }
         return false;
@@ -37,11 +51,12 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
   }
-  getCurrentUser() {
-    const user = localStorage.getItem('currentUser');
-    return user ? JSON.parse(user) : null;
+
+  getCurrentUserValue(): string | null {
+    return this.currentUserSubject.value;
   }
-  
-  
 }
+
+
