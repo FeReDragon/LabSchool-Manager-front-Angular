03c8e1b934436152d1131b794cac61a709deb8e7
“FeReDragon” <f3030ma@gmail.com>
2023-07-20 23:48:25 -0300

    register -validacao e alert

--- LABSchool_Manager/src/app/auth/auth.module.ts
+++ LABSchool_Manager/src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,6 +15,7 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     AuthRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     
   ]
 })
