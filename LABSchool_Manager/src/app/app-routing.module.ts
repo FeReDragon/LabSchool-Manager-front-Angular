294d6212cf7d91a717ca83e0e40fdc5344961b45
“FeReDragon” <f3030ma@gmail.com>
2023-07-22 23:43:54 -0300

    update rotas

--- LABSchool_Manager/src/app/app-routing.module.ts
+++ LABSchool_Manager/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from '../app/guards/auth.guard'; // Importando o authGuard
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
 
     { path: 'login', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'register', component: RegisterComponent },
 
   {
     path: 'dashboard',
@@ -21,6 +24,10 @@ const routes: Routes = [
     loadChildren: () => import('./pedagogic-support/pedagogic-support.module').then(m => m.PedagogicSupportModule),
     canActivate: [authGuard] // Adicionando o guard
   },
+  { path: 'not-found', 
+  component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
+  
  
   // outras rotas vão aqui...
 ];
--- LABSchool_Manager/src/app/auth/auth-routing.module.ts
+++ LABSchool_Manager/src/app/auth/auth-routing.module.ts
@@ -7,7 +7,7 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  // { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
