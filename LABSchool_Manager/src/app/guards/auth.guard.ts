922590895bf14a4b5a16d52e12d0cd8bf166569d
“FeReDragon” <f3030ma@gmail.com>
2023-07-25 19:19:45 -0300

    update guard-login

--- LABSchool_Manager/src/app/guards/auth.guard.ts
+++ LABSchool_Manager/src/app/guards/auth.guard.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class authGuard implements CanActivate {
+  
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor(private authService: AuthService, private router: Router) { }
-
-  canActivate(): boolean {
-    if (this.authService.isLoggedIn()) {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    if(this.authService.isLoggedIn()) {
       return true;
     } else {
-      this.router.navigate(['/auth/login']);
+      this.router.navigate(['/login']);
       return false;
     }
-  }
+  } 
 }
+
 
